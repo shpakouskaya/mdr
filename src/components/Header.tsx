2c24a61455278afc8a72c1732cdce5eb426f2dc3
lizavetashpakouskaya <elis.liza567@gmail.com>
2025-04-01 17:46:59 +0200

    Adding funny moment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,16 +3,15 @@ import * as React from "react";
 import LumonLogo from "./LumonLogo.tsx";
 
 const Header: React.FC = () => {
-    const location = "Minsk";
-    const completion = 42;
+    const funnyName = "Milkshake";
+    const completion = 0;
 
     return (
         <header className="flex w-full relative items-center justify-between py-8 px-12 border-b border-gray-600">
             <div className="header-container w-[91%] flex items-center justify-between py-1 px-2 border-2 border-r-0">
-                <div className="text-4xl">{location}</div>
+                <a className="text-4xl" target="_blank" href="https://www.youtube.com/watch?v=VngE9BiEe7Q">{funnyName}</a>
                 <div className="flex items-center space-x-4 mr-20">
                     <span className="text-lg">{completion}% Complete</span>
-                    {/*<ProgressBar value={completion} max={100} />*/}
                     <LumonLogo/>
 
                 </div>
